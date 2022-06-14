465772ac7d9c803d682df0eda3c10cd5a5e2f6f4
YeaJeong <bananana0118@gmail.com>
2022-06-14 23:51:37 +0900

    1516문제

--- kangyeajeong/BOJ_10845.py
+++ kangyeajeong/BOJ_10845.py
@@ -1,5 +1,6 @@
 
 #백준 _10845 풀이
+import sys
 
 class Queue :
     def __init__(self):
@@ -17,31 +18,31 @@ class Queue :
     def push(self, data):
         self.queue.append(data);
         self.back += 1;                     #back조정
-        self.size +=1;
+        self.size += 1;
 
     def pop(self):
         if self.isEmpty():
             return -1;
         else :
             dequeued = self.queue[0];
-            
-            self.front +=1;                 #front 조정
-            self.size -= 1;                 #size 조정
+            self.front += 1;                 #front 조정
+            self.size  -= 1;                 #size 조정
+
             self.queue = self.queue[self.front:]
-            self.front=0;
+            self.front = 0;                   #front의 index조정
             return dequeued;
 
     def getFront(self):
         if self.isEmpty():
             return -1;
         else :
-            return (self.queue[0]);
+            return self.queue[0];           #첫번쨰 원소 출력
 
     def getBack(self):
         if self.isEmpty():
             return -1;
         else:
-            return self.queue[-1];
+            return self.queue[-1];          #마지막 원소 출력
 
 
 myQueue = Queue();
@@ -49,12 +50,10 @@ myQueue = Queue();
 
 T = int(sys.stdin.readline().rstrip());
 
-
 for i in range (0,T):
     word = (sys.stdin.readline().rstrip().split());
     if(word[0] == "push"):                  #push 구현
         myQueue.push((word[1]));
-
     elif (word[0] == "front"):             
         print(myQueue.getFront());
     elif (word[0] == "back"):
